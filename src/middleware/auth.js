1d0922967e84891e6dd932e94417ef19f684d6d4
Anita <as1011930@gmail.com>
2022-04-24 22:16:21 +0530

    anita

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,20 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose');
 
+const authCheck = async (req, res, next) => {
+    try{
+    let id = req.params.userId
+    if(!mongoose.isValidObjectId(req.params.userId)) return res.send({msg: "Invalid UserId sent in Params"})
+    let token = req.headers['x-Auth-Token'] || req.headers['x-auth-token']
+    if(!token) return res.send({msg: 'Token is required.'}) 
+    let tokenValidity = jwt.verify(token, "Anita")
+    if(tokenValidity.userId !== id) return res.send({msg: "This user hasn't been Authorised"}) 
     next()
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(500).send({status: false, msg: 'invalid token, unable to verify user. Please re-log in.'}) 
+    }
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports = {authCheck}
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,21 +1,24 @@
+
+
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
-    mobile: {
-        type: String,
-
-        required: true
-    },
     emailId: String,
     password: String,
+    mobile: String,
     gender: {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
     age: Number,
-    posts: {type: [], deafult: []}
-}, { timestamps: true });
 
+},
+    { timestamps: true });
+    
 module.exports = mongoose.model('User', userSchema)
