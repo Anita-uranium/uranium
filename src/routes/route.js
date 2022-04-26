1e88cd16af3d5c40af3c0fbb6e79c0ec8449e085
Anita <as1011930@gmail.com>
2022-04-26 11:29:45 +0530

    25th april assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,14 +9,8 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-
-
+router.get("/appointment/sessions", CowinController.getByDistrictId)
+router.get("/weather", CowinController.weather)
+router.get("/cities",CowinController.getSortedCities)
+router.post("/memes",CowinController.memes)
 module.exports = router;
